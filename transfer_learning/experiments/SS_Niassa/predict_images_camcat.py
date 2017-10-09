11d2d3fb2aa0652f138b540183efe22a740a91d6
Marco Willi <will5448@umn.edu>
2017-10-08 23:00:45 -0500

    bug

--- transfer_learning/experiments/SS_Niassa/predict_images_camcat.py
+++ transfer_learning/experiments/SS_Niassa/predict_images_camcat.py
@@ -6,15 +6,16 @@ def predict_images():
     """ Predict Images in Directory """
 
     # class mapping according to model
-    model_cfg_jsons = ['/host/data_hdd/save/camera_catalogue' +
+    model_cfg_jsons = ['/host/data_hdd/save/camera_catalogue/' +
                        'cc_species_v2_201708210308_cfg.json',
-                       '/host/data_hdd/save/camera_catalogue' +
+                       '/host/data_hdd/save/camera_catalogue/' +
                        'cc_blank_vehicle_species_v2_201708200608_cfg.json']
 
     # score images
-    model_files = ['/host/data_hdd/save/ss/ss_species_51_201708072308.hdf5',
-                   '/host/data_hdd/models/ss/' +
-                   'ss_blank_vs_non_blank_small_201707172207_model_best.hdf5']
+    model_files = ['/host/data_hdd/save/camera_catalogue/' +
+                   'cc_species_v2_201708210308.hdf5',
+                   '/host/data_hdd/save/camera_catalogue/' +
+                   'cc_blank_vehicle_species_v2_201708200608.hdf5']
 
     # input images for scoring
     pred_path = '/host/data_hdd/images/niassa/'
