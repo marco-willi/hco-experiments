92722c03dc6adcecb779f6fc99f198da636e9a17
Marco Willi <will5448@umn.edu>
2017-10-11 00:57:33 -0500

    agg

---
+++ transfer_learning/experiments/camera_catalogue_south_africa_4/aggregate_classifications.py
@@ -0,0 +1,181 @@
+"""
+Code to process data dumps from panoptes
+"""
+from config.config import cfg_path
+import os
+import pandas as pd
+import json
+import numpy as np
+from collections import Counter, OrderedDict
+import pickle
+import random
+from db.data_prep_functions import *
+from datetime import datetime
+import re
+import csv
+
+
+cls =  read_classification_data_camcat(cfg_path['db'] +\
+    'classifications_experiment_20171008.csv')
+cls[list(cls.keys())[0]]
+# dict_keys(['', 'classification_id', 'user_name', 'user_id', 'user_ip',
+#  'workflow_id', 'workflow_name', 'workflow_version', 'created_at',
+#  'gold_standard', 'expert', 'metadata', 'annotations', 'subject_data',
+#  'subject_ids'])
+
+# extract subject data from classification data
+subs_all = OrderedDict()
+for k, v in cls.items():
+    json_extract = json.loads(v['subject_data'])
+    key = list(json_extract.keys())[0]
+    subs_all[v['subject_ids']] = json_extract[key]
+
+# loop through all classifications and fill subject dictionary
+subs_res = OrderedDict()
+for k, v in cls.items():
+    # get subject id
+    current_c = v['subject_ids']
+    # get workflow_id
+    workflow_id = v['workflow_id']
+    # create subject entry if not in dictionary
+    if current_c not in subs_res:
+        subs_res[current_c] = {workflow_id: {'users': dict(),
+                                             'retirement_reason': ''}}
+    elif workflow_id not in subs_res[current_c]:
+        subs_res[current_c][workflow_id] = {'users': dict(),
+                                            'retirement_reason': ''}
+    # current subject workflow
+    sub_wf_current = subs_res[current_c][workflow_id]
+    # retirement
+    ret = json.loads(v['subject_data'])
+    key = list(ret.keys())[0]
+    if ret[key]['retired'] is None:
+        ret_res = 'Not Retired'
+    else:
+        ret_res = ret[key]['retired']['retirement_reason']
+    # classifications
+    cl_extract = json.loads(v['annotations'])[0]['value']
+    # map answer based on workflow_id
+    if workflow_id == '5001':
+        mapper = {'Yes': 'novehicle', 'No': 'vehicle'}
+        try:
+            cl_extract = mapper[cl_extract]
+        except:
+            pass
+    elif workflow_id == '5000':
+        mapper = {'Yes': 'notblank', 'No': 'blank'}
+        try:
+            cl_extract = mapper[cl_extract]
+        except:
+            pass
+    if type(cl_extract) not in (list, str):
+        cls_usr = [str(cl_extract)]
+    elif 'choice' in cl_extract[0]:
+        cls_usr = [x['choice'] for x in cl_extract]
+    else:
+        cls_usr = [cl_extract]
+    # create dictionary for user
+    if v['user_name'] not in sub_wf_current['users']:
+        sub_wf_current['users'][v['user_name']] = dict()
+    # add all classifications / species to subject/user combination
+    for cl in cls_usr:
+        sub_wf_current['users'][v['user_name']][cl] = 1
+    sub_wf_current['retirement_reason'] = ret_res
+
+subs_res[list(subs_res.keys())[0]]
+
+# randomly show some results
+keys = np.random.choice(list(subs_res.keys()), size=10)
+for k in keys:
+    print("Key: %s" % k)
+    subs_res[str(k)]
+
+# generate plurality algorithm result
+subs_res_final = OrderedDict()
+for k_sub, v_sub in subs_res.items():
+    # loop over all workflows
+    for k, v in v_sub.items():
+        # blanks
+        blank_classes = ['nothing_here', 'VEGETATIONNOANIMAL',
+                         'NOTHINGHERE', 'NTHNGHR', 'blank']
+        # check retirement reason
+        if v['retirement_reason'] in ['Not Retired', 'classification_count',
+                                      'consensus', 'other', None]:
+            label = 'unkwnown'
+        else:
+            label = v['retirement_reason']
+        # retirement status
+        if v['retirement_reason'] not in ['Not Retired', None]:
+            retirement_status = 'retired'
+        else:
+            retirement_status = 'not_retired'
+        users = v['users']
+        # calculate different species
+        n_species_user = list()
+        species_all = list()
+        for u in users.keys():
+            n_species_user.append(len(users[u]))
+            species_all.extend(list(users[u].keys()))
+        n_species_med = np.median(n_species_user)
+        # rank species
+        top_n = Counter(species_all).most_common(int(n_species_med))
+        # extract label
+        label_plur = [x[0] for x in top_n]
+        if label == 'unkwnown':
+            label_final = label_plur
+        else:
+            label_final = label
+        if type(label_final) is not list:
+            label_final = [label_final]
+        for i in range(0, len(label_final)):
+            if label_final[i] == 'human':
+                label_final[i] = 'HUMAN'
+            elif label_final[i] in blank_classes:
+                label_final[i] = 'blank'
+        if k_sub not in subs_res_final:
+            subs_res_final[k_sub] = dict()
+        subs_res_final[k_sub][k] = {
+            'ret_label': label,
+            'retirement_reason': v['retirement_reason'],
+            'plur_label': label_plur,
+            'n_species': n_species_med,
+            'label': label_final,
+            'n_users': len(users.keys()),
+            'pielou': calc_pielu(species_all, blank_classes)}
+
+subs_res_final[list(subs_res_final.keys())[0]]
+# randomly show some results
+# Key: 13172213
+keys = np.random.choice(list(subs_res_final.keys()), size=10)
+for k in keys:
+    print("Key: %s" % k)
+    subs_res_final[str(k)]
+
+# prepare final dictionary that contains all relevant data
+subs_all_data = OrderedDict()
+remove_labels = ['1', '0', 'unknown answer label', 'unknown']
+for k, v in subs_all.items():
+    if k in subs_res_final:
+        subs_all_data[k] = subs_res_final[k]
+    subs_all_data[k]['meta_data'] = v
+
+# randomly show some results
+# Key: 13172213
+keys = np.random.choice(list(subs_all_data.keys()), size=10)
+for k in keys:
+    print("Key: %s" % k)
+    subs_all_data[str(k)]
+
+# save as csv
+with open(cfg_path['db'] + 'classifications_experiment_20171008_converted.csv',
+          'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile, delimiter=',',
+                           quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csvwriter.writerow(['subject_id', 'attr', 'value'])
+    for k, v in subs_all_data.items():
+        for kk, vv in v.items():
+            for kkk, vvv in vv.items():
+                if type(vvv) is list:
+                    vvv = vvv[0]
+                row = [k, kk, kkk, str(vvv)]
+                csvwriter.writerow(row)
---
+++ transfer_learning/experiments/camera_catalogue_south_africa_4/get_classifications_from_panoptes.py
@@ -0,0 +1,104 @@
+"""
+Functions to access data via Panoptes api
+- init_panoptes
+    connect to panoptes and return project object
+- get_classifications
+    gets classifications of a project
+- get_subject_info
+    gets subjects of a project
+- download_dic
+"""
+# import modules
+from panoptes_client import Project, Panoptes
+from config.config import config, cfg_path, config_credentials
+import urllib.request
+import pandas as pd
+import json
+import time
+
+# Parameters
+project_id = 825
+
+# connect to Panoptes and return project
+def init_panoptes(Panoptes=Panoptes, config_credentials=config_credentials,
+                  project_id=project_id):
+    # connect to panoptes
+    Panoptes.connect(username=config_credentials['Zooniverse']['username'],
+                     password=config_credentials['Zooniverse']['password'])
+    # get my project
+    project_id = int(project_id)
+    my_project = Project.find(id=project_id)
+
+    return my_project
+
+
+# function to get classification data
+def get_classifications(my_project, new=False):
+    """ Function to get classifications """
+    # create new export
+    if new:
+        my_project.generate_export('classifications')
+    # read csv
+    cls = my_project.get_export('classifications')
+    cls_df = pd.read_csv(cls.url)
+    return cls_df
+
+
+# function to get subject infos
+def get_subject_info(my_project, new=False):
+    """ Function to get subject infos (links) """
+    # create new export
+    if new:
+        my_project.generate_export('subjects')
+    # read csv
+    subs = my_project.get_export('subjects')
+    subs_df = pd.read_csv(subs.url)
+    # subject ids / urls / metadata
+    subject_ids = subs_df['subject_id']
+    subject_urls = [json.loads(x)['0'] for x in subs_df['locations']]
+    subject_meta = [json.loads(x) for x in subs_df['metadata']]
+    # fill dictionary
+    subs_dir = dict()
+    for i in range(0, len(subject_ids)):
+        subs_dir[subject_ids[i]] = {'url': subject_urls[i],
+                                    'metadata': subject_meta[i]}
+    return subs_dir
+
+
+# function to download from dictionary
+def download_dic(dat, path, n=-1):
+    time_start = time.time()
+    i = 0
+    for sub_id, sub_url in dat.items():
+        i += 1
+        urllib.request.urlretrieve(sub_url, path + str(sub_id) + ".jpeg")
+        if n == i:
+            break
+    time_end = time.time()
+    time_diff = round(time_end - time_start, 2)
+    print("Required %s seconds to read %s images" % (time_diff, i))
+
+
+if __name__ == '__main__':
+    my_project = init_panoptes()
+
+    # get classifications and subject links
+    cls = get_classifications(my_project)
+
+    # get subject data
+    # subs = get_subject_info(my_project)
+
+    # subselect
+    workflow_ids = [5000, 5001, 4963]
+    cls_filtered = cls[cls.workflow_id.isin(workflow_ids)]
+    cls_filtered.to_csv(cfg_path['db'] +\
+        'classifications_experiment_20171008.csv')
+
+    # save to disk
+    subs = get_subject_info(my_project)
+
+    # path to download to
+    path_images = config['paths']['path_test_downloads']
+
+    # download images from dictionary
+    download_dic(dat=subs, path=path_images, n=10)
